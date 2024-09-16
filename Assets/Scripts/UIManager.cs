a1ab814f622657d01e46a66f430fffcdc8837314
MxxMkk <154011817+byteauthority@users.noreply.github.com>
2024-09-16 17:13:54 +0200

    Buy Menu
    Button that shows buy menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject BuyUI;
+
+    public void ShowUI()
+    {
+        BuyUI.SetActive(true);
+    }
+    
+}
