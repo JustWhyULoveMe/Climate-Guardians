badc79bb456d486a2566dd65c53248f542e39b50
MxxMkk <154011817+byteauthority@users.noreply.github.com>
2024-09-18 20:01:55 +0200

    Camera movement (not finished)

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private Vector3 Origin;
+    private Vector3 Difference;
+    private Vector3 ResetCamera;
+
+    private bool drag = false;
+
+    // Public boundaries for camera movement (adjustable in the Unity Inspector)
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private void Start()
+    {
+        ResetCamera = Camera.main.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            if (!drag)
+            {
+                drag = true;
+                Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            drag = false;
+        }
+
+        if (drag)
+        {
+            Vector3 newPosition = Origin - Difference;
+
+            // Clamp the camera's position to stay within the boundaries
+            newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+            newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+
+            Camera.main.transform.position = newPosition;
+        }
+
+        // Reset the camera position with the right mouse button
+        if (Input.GetMouseButton(1))
+        {
+            Camera.main.transform.position = ResetCamera;
+        }
+    }
+}
