c2a79d54c12cf49a931a0924b061d56a12074ad7
MxxMkk <154011817+byteauthority@users.noreply.github.com>
2024-10-10 08:22:31 +0200

    Polution-Bar

--- Assets/Scripts/StatsController.cs
+++ Assets/Scripts/StatsController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,15 +6,76 @@ using UnityEngine.UI;
 public class StatsController: MonoBehaviour
 {
     private Image _image;
-    [SerializeField] private Gradient barGradient; 
+    [SerializeField] private Gradient barGradient;
+
+    public float maxTBar = 10f;
+    public float trashAmount = 1f;
+
     private void Start()
     {
         _image = GetComponent<Image>();
+        CheckBarGradient();
+
     }
 
-    public void UpdateBar(float maxBar, float currentBar)
+    public void LowTrashLevel()
     {
-        _image.fillAmount = currentBar / maxBar;
+        trashAmount = trashAmount + 0.1f;
+        CheckBarGradient();
     }
 
+    public void CheckBarGradient()
+    {
+        _image.color = barGradient.Evaluate(trashAmount);
+        _image.fillAmount = trashAmount;
+    }
+
+
+
+
+    
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsController : MonoBehaviour
+{
+    public Image filledBar;
+
+    public Gradient gradient;
+
+    public float minTrash, maxTrash;
+
+    private float currentTrash;
+
+    private void Start()
+    {
+
+        currentTrash = maxTrash / 10;
+
+        UpdateUI();
+    }
+
+    public void Clean(float points)
+    {
+        currentTrash = Mathf.Clamp(currentTrash - points, minTrash, maxTrash);
+        UpdateUI();
+    }
+
+    public void Polute(float points)
+    {
+        currentTrash = Mathf.Clamp(currentTrash + points, minTrash, maxTrash);
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        filledBar.fillAmount = (float)currentTrash / maxTrash;
+
+        filledBar.color = gradient.Evaluate(filledBar.fillAmount);
+    }
 }
