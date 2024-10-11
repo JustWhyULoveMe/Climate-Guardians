09cd318ff41849e8148ff455a0d08d06bfc05e7e
MxxMkk <154011817+byteauthority@users.noreply.github.com>
2024-10-11 14:34:26 +0200

    stats-almost

--- Assets/QuestSys/CityEconomyManager.cs
+++ Assets/QuestSys/CityEconomyManager.cs
@@ -1,13 +1,20 @@
 // CityEconomyManager.cs
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CityEconomyManager : Singleton<CityEconomyManager>
 {
     private int money;
 
+    public Text moneyDisplay;
+
     public void AddMoney(int amount)
     {
         money += amount;
+        moneyDisplay.text = money.ToString();
         // Update city economy, etc.
     }
+
+
+
+
 }
